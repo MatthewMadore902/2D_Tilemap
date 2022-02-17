171d2b4fd772850215fa309b8f5686fd444e2ac3
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2022-02-17 11:32:35 -0400

    Initial commit

---
+++ Assets/2d_CharaterController_NonAnimated/PlayerMovement_2D.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_2D : MonoBehaviour
+{   
+    [Header("Input settings:")]
+    public float speedMultiplier = 5.0f;
+
+    [Space]
+    [Header("Character Stats:")]
+    public Vector2 movementDirection;
+    public float movementSpeed;
+
+    [Space]
+    [Header("References:")]
+    public Rigidbody2D rb;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+        Move();
+      
+
+
+    }
+
+    void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed * speedMultiplier;
+    }
+
+   
+
+}
